7eab6bfa0f7972519db79cc5aee77300eba38d0f
Slamihin Egor <slamihin123@gmail.com>
2021-08-11 00:03:30 +0300

    first commit

---
+++ lib/elixir_todo_traning.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTodoTraning do
+  @moduledoc """
+  Documentation for `ElixirTodoTraning`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTodoTraning.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_todo_traning_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirTodoTraningTest do
+  use ExUnit.Case
+  doctest ElixirTodoTraning
+
+  test "greets the world" do
+    assert ElixirTodoTraning.hello() == :world
+  end
+end
